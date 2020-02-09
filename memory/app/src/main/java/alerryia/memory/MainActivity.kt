24a03fba9472860c594906bb4e9f97ffd5df9b13
Valeryia Shekhautsova <alerryia@gmail.com>
2020-02-09 17:57:55 +0300

    startapp dev

---
+++ memory/app/src/main/java/alerryia/memory/MainActivity.kt
@@ -0,0 +1,12 @@
+package alerryia.memory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
