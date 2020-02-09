a0e02d57475615ec800bd9f6da74015e8220f921
Valeryia Shekhautsova <alerryia@gmail.com>
2020-02-09 18:30:56 +0300

    add pictures

--- memory/app/src/main/java/alerryia/memory/MainActivity.kt
+++ memory/app/src/main/java/alerryia/memory/MainActivity.kt
@@ -2,6 +2,7 @@ package alerryia.memory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
